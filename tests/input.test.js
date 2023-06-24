d4077df5eb8842c1e7718d69f4889690476ce7cb
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-06-24 14:31:41 -0400

    added a test case for checking if the title is valid

--- tests/input.test.js
+++ tests/input.test.js
@@ -1,17 +1,14 @@
-// condition ? expression1 : expression2;
-
-// var age = 20;
-// var message = (age >= 18) ? "You are an adult" : "You are a minor";
-// console.log(message);
 
 const Cli = require('../lib/input');
 const inquirer = require('inquirer');
 
+//test case fo the title
 describe('prompt title', () => {
+    //since the promptTitle() is an asynchronous fucntion, we have to use async and await
     it('should throw an error if title is more than 3 chars', async () => {
 
         const title = new Cli();
-        const input = 'reds';
+        const input = 'red';
 
         //This allows you to control the behavior of the prompt and provide mock responses.
         inquirer.prompt = jest.fn();
